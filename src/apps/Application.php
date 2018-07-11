a168fdf76c3b6fed5575abf2211cd5bb6c0695d4
Hilkiah Makemo <hilmak01@gmail.com>
2018-07-11 17:21:53 -0600

    first commit

---
+++ src/apps/Application.php
@@ -0,0 +1,9 @@
+<?php
+namespace SKYW\Upgrader\Apps;
+
+use Symfony\Component\Console\Application as SymApp;
+
+class Application extends SymApp
+{
+  
+}
---
+++ src/cmds/Command.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SKYW\Upgrader\Cmds;
+
+use Symfony\Component\Console\Command\Command as SymCommand;
+
+class Command extends SymCommand
+{
+    
+}
