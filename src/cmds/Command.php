84f7088af5af3eba5904dc3c920003329418eab6
Hilkiah Makemo <hilmak01@gmail.com>
2018-07-11 18:03:58 -0600

    updated cmds and apps

--- src/cmds/Command.php
+++ src/cmds/Command.php
@@ -2,9 +2,16 @@
 
 namespace SKYW\Upgrader\Cmds;
 
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command as SymCommand;
 
 class Command extends SymCommand
 {
-    
+    public function all()
+    {
+       return get_object_functions($this);
+    }
 }
